e1c83d0772bff45893e8ceb794aca16de75b2290
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-10-30 00:40:45 +0530

    Add counter functionality with increment, decrement, and reset buttons
    - Updated App.js to include state management for a counter
    - Added buttons for incrementing, decrementing, and resetting the counter
    - Enhanced styling in App.css for button layout and appearance

--- react_and_tailwind/react_chrome_extension/src/App.js
+++ react_and_tailwind/react_chrome_extension/src/App.js
@@ -1,10 +1,41 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+  const handleSubtract = () => {
+    setCount(count - 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
   return (
     <>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-      <p>This is a React Chrome Extension</p>
+      <h1 className="text-5xl font-bold text-blue-500 mt-3">{count}</h1>
+      <div>
+        <button
+          className="btn bg-red-500 hover:bg-red-700"
+          onClick={handleSubtract}
+        >
+          -
+        </button>
+        <button
+          className="btn bg-blue-500 hover:bg-blue-700"
+          onClick={handleAdd}
+        >
+          +
+        </button>
+      </div>
+      <button
+        className="btn bg-gray-500 hover:bg-gray-700"
+        onClick={handleReset}
+      >
+        Reset
+      </button>
     </>
   );
 }
